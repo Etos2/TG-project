c9f58fcb0907c29aa933453fd45513223ce24a14
Etos2 <43020478+Etos2@users.noreply.github.com>
2018-09-18 12:08:39 +1000

    Update master.py

--- master.py
+++ master.py
@@ -1,1 +1,14 @@
-git gud
+from Tkinter import *
+
+#Load file
+tg = {}
+try:
+    for entry in open("Class10w.txt"):
+        entry.rsplit()
+        student, attendance = entry.split(":")
+        tg[student] = int(attendance) #int to catch errors
+    #debug
+    print(tg)
+
+except:
+    print("Error: Failed to load the attendance")
